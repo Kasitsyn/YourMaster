5ab76773dd16b1302a3a87d715e480c05d1677e7
kasitsyn <kasitsyn@gmail.com>
2022-04-01 17:24:10 +0700

    refactor: replace render with createRoot for react 18 needed

--- app/src/index.js
+++ app/src/index.js
@@ -3,12 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as ReactDOMClient from 'react-dom/client';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const container = document.getElementById('root');
+const root = ReactDOMClient.createRoot(container);
+
+root.render(<App />)
 
 reportWebVitals();

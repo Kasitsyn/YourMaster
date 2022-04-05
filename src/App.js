e29f206a71ecb9ed168eef8466cfb1e91a2018a7
kasitsyn <kasitsyn@gmail.com>
2022-04-05 14:27:36 +0700

    feat: create Image component and some refactor Icon, Button, Footer components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
 import { Footer } from './components/Footer/Footer';
 import { Header } from './components/Header/Header';
+import { Image } from './components/UI/Img/Img';
 
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Footer/>
-      
+      <Header />
+      <Footer />
+      <Image
+        src={require("./img/star.png")}
+        alt={'IMG'}
+        width={200}
+        height={200} />
     </div>
   );
 }
--- src/components/UI/Icon/Icon.jsx
+++ src/components/UI/Icon/Icon.jsx
@@ -2,10 +2,29 @@ import classNames from "classnames";
 import "./Icon.css";
 
 export function Icon({ name, onClick, className, size, disabled, ...attrs }) {
-  const classes = classNames("fa", `fa-${name}`, className, {func: onClick}, {disabled});
-  const elemSize = size ? {fontSize: `${size}rem`} : null
-  
+  const classes = classNames(
+    "fa",
+    `fa-${name}`,
+    className,
+    { func: onClick },
+    { disabled }
+  );
+  const elemSize = size ? { fontSize: `${size}rem` } : null;
+
   return (
-    <i {...attrs} style={elemSize} className={classes} disabled={disabled ? null : onClick} onClick={onClick} />
+    <i
+      {...attrs}
+      style={elemSize}
+      className={classes}
+      onClick={disabled ? null : onClick}
+    />
   );
 }
+
+Icon.defaultProps = {
+  name: "",
+  onClick: null,
+  disabled: false,
+  size: null,
+  className: "",
+};
